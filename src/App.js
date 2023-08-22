ab93624eb8420e464d9ad6f5946b34379ffbadc7
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-22 20:07:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import OrderProvider from './Store/OrderProvider';
+import CandyForm from './Components/Candy/CandyForm';
+import CandyList from './Components/Candy/CandyList';
+import Header from './Components/Header/Header';
+import Cart from './Components/Cart/Cart';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [ShowCart,setShowCart]=useState(false)
+
+  const ShowCartHandler=()=>{
+    setShowCart(true)
+  }
+
+  const CancelCartHandler=()=>{
+    setShowCart(false)
+  }
+  
+
+  return(
+    <OrderProvider>
+     <header>
+         <Header onDisplay={ShowCartHandler} />
+     </header>
+     <main>
+        <CandyForm />
+        <CandyList />
+     </main>
+     {ShowCart && <Cart onCancel={CancelCartHandler}/> }
+    </OrderProvider>
+  )
+  
 }
 
 export default App;
